8a9965facfe06fc0fa5dc22b7feb3d848644c5a8
orkhanibrahim <orkhankhazar@gmail.com>
2022-03-08 03:25:06 +0400

    modified csv file batch configuration added to read the whole data in csv file and write it to databse

--- src/main/java/com/orxanibrahim/tsldasboard/model/Match.java
+++ src/main/java/com/orxanibrahim/tsldasboard/model/Match.java
@@ -3,18 +3,22 @@ package com.orxanibrahim.tsldasboard.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import java.time.LocalDate;
 
 @Getter
 @Setter
+@Entity
 public class Match {
+    @Id
     private int id;
     private LocalDate date;
-    private LocalDate year;
-    private LocalDate week;
+    private String year;
+    private String week;
     private String homeTeam;
     private String visitorTeam;
-    private int fullTimeResult;
+    private String fullTimeResult;
     private String homeTeamGoal;
     private String visitorTeamGoal;
     private String halfTimeResult;
