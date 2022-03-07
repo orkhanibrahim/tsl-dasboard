8485162a73ed0fa6372615b44e7b25f2d4f3b79e
orxanibrahim <orkhankhazar@gmail.com>
2022-03-07 23:28:38 +0400

    initial commit

---
+++ src/main/java/com/orxanibrahim/tsldasboard/TslDasboardApplication.java
@@ -0,0 +1,13 @@
+package com.orxanibrahim.tsldasboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TslDasboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TslDasboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/orxanibrahim/tsldasboard/TslDasboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orxanibrahim.tsldasboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TslDasboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
